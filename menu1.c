8e0ec7e0a66c8016a1897f39ec850cfbf1df3f16
Nocasis <nocasisn@gmail.com>
2017-08-21 21:10:07 +0700

    Test

---
+++ menu1.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+const int menucount = 4;
+
+#define ESC 27
+#define W 119
+#define A 97
+#define S 115
+#define D 100
+#define SPACE 32
+#define ENTER 10 
+#define TAB 9
+
+
+int mygetch( ) {
+    struct termios oldt,
+                   newt;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
+}
+
+
+
+void menulist(int a)
+{
+    switch(a)
+            {
+                case 0:
+                        printf("1.New Game<<<\n2.Select Map \n3.About \n4.Exit\n5.Test\n");
+                        break;
+                 case 1:
+                        printf("1.New Game\n2.Select Map<<< \n3.About \n4.Exit\n5.Test\n");
+                        break;
+                case 2:
+                        printf("1.New Game\n2.Select Map \n3.About<<< \n4.Exit\n5.Test\n");
+                        break;
+            case 3:
+                        printf("1.New Game\n2.Select Map \n3.About \n4.Exit<<<\n5.Test\n");
+                        break;
+            case 4:
+                        printf("1.New Game\n2.Select Map \n3.About \n4.Exit\n5.Test<<<\n");
+                        break;
+            }
+}
+
+
+
+int menu(int a)
+{
+    char temp;
+    while(1)
+    {
+        system("clear");
+        printf("\t\t The Game \n\n");
+        menulist(a);
+        temp=mygetch();
+        switch (temp)
+        {
+            case ENTER:
+                return a; 
+            case W:
+                if((a<=0)||(a>menucount))
+                    a=menucount;
+                else
+                    a--;
+                break;
+            case S:
+                if((a<0)||(a>=menucount))
+                    a=0;
+                else
+                    a++;
+        }
+    }
+}
+
+int main()
+{
+    int Select=0,level=1;
+    while(1)
+    {
+        Select=menu(Select);
+        switch (Select)
+        {
+            case 0:
+                system("clear");
+                printf("Test1");
+                mygetch();
+                break;
+            case 1:
+                system("clear");
+                printf("Test2");
+                mygetch();
+                break;
+            case 2:
+                system("clear");
+                printf("Test3");
+                mygetch();
+                break;
+            case 3:
+                system("clear");
+                return 0;
+            case 4:
+                system("clear");
+                printf("Developers");
+                mygetch();
+                break;
+            default:
+                    printf("Error");
+                    mygetch();
+                    return 0;
+        }
+    }
+    mygetch();
+    return 1;
+}
