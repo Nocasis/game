e2367ec1a90d934a2387bf5894beb0d4d6b6535b
Nocasis <nocasisn@gmail.com>
2017-08-22 18:43:16 +0700

    fixes

--- main.c
+++ main.c
@@ -13,7 +13,22 @@ const int menucount = 5;
 #define ENTER 10 
 #define TAB 9
 
-#include "func.c"
+#include "main_menu.c"
+
+int getch( ) 
+{
+    struct termios oldt,
+                   newt;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
+}
+
 
 int main()
 {
--- main_menu.c
+++ main_menu.c
@@ -1,17 +1,3 @@
-int getch( ) 
-{
-    struct termios oldt,
-                   newt;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldt );
-    newt = oldt;
-    newt.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-    return ch;
-}
-
 //work with maps
 
 
